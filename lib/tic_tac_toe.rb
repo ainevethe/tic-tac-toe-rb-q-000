daf803f242ba4dda663e3dd6efc09cc0f7b0db62
Crista Cooper <aine3d@gmail.com>
2017-10-15 22:19:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -135,7 +135,7 @@ def winner(board)
   if !(won?(board))
     return nil
   elsif board[won?(board)[0]] == "X"
-    return "X"
+    return "Congratulations X!"
   else board[won?(board)[0]] == "O"
     return "O"
   end
