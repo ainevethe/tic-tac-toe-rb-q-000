3e50a89276d396eb83eb1dea2f58a2ef74c822af
Crista Cooper <aine3d@gmail.com>
2017-10-15 20:56:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,26 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def turn_count(board)
+
+  counter = 0
+
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter +=1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else turn_count(board) % 2 != 0
+    return "O"
+  end
+end
+
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
