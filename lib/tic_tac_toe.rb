1dc826cd0a03eb9351ef93c23b1ce7c1aab8b924
Crista Cooper <aine3d@gmail.com>
2017-10-15 21:04:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,10 +10,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
