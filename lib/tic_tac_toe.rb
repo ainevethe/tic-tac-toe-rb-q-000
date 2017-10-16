ed022ece489b12ad73312c531fcb42aff4594751
Crista Cooper <aine3d@gmail.com>
2017-10-16 00:37:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -146,13 +146,12 @@ end
 
 def play(board)
   turns = 0
-    until won?(board) == true
       until over?(board) == true || won?(board) == true || draw?(board) == true
         turn(board)
         turns += 1
       end
     end
-    if won?(board) != false
+    if won?(board) == true
       if board[won?(board)[0]] == "X"
         puts "Congratulations X!"
       else
